ca35555af3538b734cbd99ad3848cc223ceab434
Chen <neil_chen_0918@live.cn>
2021-03-16 16:26:54 +0800

    upgrade to angular11

--- src/Netcool.Admin/ClientApp/src/app/routes/routes-routing.module.ts
+++ src/Netcool.Admin/ClientApp/src/app/routes/routes-routing.module.ts
@@ -52,11 +52,12 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(
       routes, {
-        useHash: environment.useHash,
-        // NOTICE: If you use `reuse-tab` component and turn on keepingScroll you can set to `disabled`
-        // Pls refer to https://ng-alain.com/components/reuse-tab
-        scrollPositionRestoration: 'top',
-      }
+    useHash: environment.useHash,
+    // NOTICE: If you use `reuse-tab` component and turn on keepingScroll you can set to `disabled`
+    // Pls refer to https://ng-alain.com/components/reuse-tab
+    scrollPositionRestoration: 'top',
+    relativeLinkResolution: 'legacy'
+}
     )],
   exports: [RouterModule],
 })
