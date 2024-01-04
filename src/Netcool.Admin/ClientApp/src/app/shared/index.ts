61496a18eca2545f938ed5b6367e94985da94cca
Chen <neil_chen_0918@live.cn>
2024-01-04 11:00:34 +0800

    remove json-schema.module

--- src/Netcool.Admin/ClientApp/src/app/shared/index.ts
+++ src/Netcool.Admin/ClientApp/src/app/shared/index.ts
@@ -6,6 +6,5 @@ export * from './utils/yuan';
 // Module
 export * from './shared.module';
 export * from './cell-widget/index'
-export * from './json-schema/json-schema.module';
 export * from './json-schema/index';
 export * from './st-widget/index';
